f0df03e5b1c65aee04def4d89d6dadd61006cfcc
therealPatrick <malamashangala13@gmail.com>
2024-08-14 15:53:31 +0200

    working on dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
 	include Pundit
+
+  def after_sign_in_path_for(resource)
+    tasks_path  # Redirects to the tasks index page after sign-in
+  end
 end
